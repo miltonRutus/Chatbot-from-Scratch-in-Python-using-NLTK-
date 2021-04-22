d8ba952f425430c25ee1c0bc963adfc3367021ea
milton rustrian <miltonrutus21@gmail.com>
2021-04-22 15:58:13 -0600

    install

---
+++ install.py
@@ -0,0 +1,10 @@
+import nltk
+from nltk.stem import WordNetLemmatizer
+nltk.download('popular', quiet=True) # for downloading popular packages
+nltk.download('punkt')
+nltk.download('wordnet')
+
+
+#intalar
+#pip install numpy
+#pip install -U scikit-learn
